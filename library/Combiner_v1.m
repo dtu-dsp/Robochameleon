e4c4c51b45ca18667177eb8970d5bc8d3f5d031a
Molly Piels <mopi@fotonik.dtu.dk>
2016-09-05 16:15:23 +0200

    More versatile combiner

--- library/Combiner_v1.m
+++ library/Combiner_v1.m
@@ -7,39 +7,52 @@
 %> Set type to 'simple' to combine N signals to N columns, or 'complex' to
 %> combine N signals to N/2 complex colums
 %>
-%> Example:
+%> __Example__
 %> @code
-%> combiner = Combiner_v1(2);
+%>   param.cb.nInputs = 2;
+%>   param.cb.type = 'simple';
+%>  combiner = Combiner_v1(param.cb);
 %> @endcode
 %>
 %> @see Splitter_v1
 %>
 %> @author Miguel Iglesias
+%> @author Simone Gaiarin
+%>
 %> @version 1
 classdef Combiner_v1 < unit
-
+    
     properties
         %> Number of input signals
-        nInputs; 
+        nInputs;
         %> Number of output signals
-        nOutputs = 1; 
+        nOutputs = 1;
         
-        %> Combiner type {'simple' | 'complex'}
-        type; 
+        %> Combiner type {'simple' | 'complex' | 'add' | 'complexInterleave'}
+        type;
     end
     
     methods
         %> @brief Class constructor
         %>
-        %> @param type Combiner type {'simple' | 'complex'}
-        %> @param nInputs number of inputs
-        %> @return instance of the Combiner_v1 class
-        function obj = Combiner_v1(type, nInputs)
-            obj.type = type;
-            if strcmp(obj.type, 'complex') && rem(nInputs,2) ~= 0
+        %> @param param.type Combiner type {'simple' | 'complex'}
+        %> @param param.nInputs Number of inputs
+        function obj = Combiner_v1(varargin)
+            if length(varargin) > 1
+                param.type = varargin{1};
+                param.nInputs = varargin{2};
+                robolog('This interface is deprecated and will be removed. Pass a struct with parameters instead.', 'WRN');
+            else
+                param = varargin{1};
+            end
+            
+            if strcmp(obj.type, 'complex') && rem(param.nInputs,2) ~= 0
                 robolog('For complex operation nInputs must be even', 'ERR');
             end
-            obj.nInputs = nInputs;
+            if strcmp(obj.type, 'complexInterleave') && param.nInputs ~= 2
+                robolog('For complex interleaving operation nInputs must be two', 'ERR');
+            end
+            obj.setparams(param);
         end
         
         %> @brief Traverse function
@@ -47,9 +60,19 @@ classdef Combiner_v1 < unit
         %> @param varargin inputs
         %> @return out output
         function out = traverse(obj,varargin)
-            out = obj.combine(varargin);
-            if strcmp(obj.type, 'complex')
-                out = obj.combineComplex(out);
+            switch obj.type
+                case 'simple'
+                    out = obj.combine(varargin);
+                case 'complex'
+                    out = obj.combine(varargin);
+                    out = obj.combineComplex(out);
+                case 'add'
+                    out = varargin{1};
+                    for jj = 2:nargin-1
+                        out = out + varargin{jj};
+                    end
+                case 'complexInterleave'
+                    out = varargin{1} + varargin{2}*1i;
             end
         end
     end
@@ -93,12 +116,12 @@ classdef Combiner_v1 < unit
         %> Takes a cell array of signal_interface and makes sure relevant
         %> parameters match
         function CheckInputs(in)
-            Rs =cellfun(@(x)x.Rs, in); 
+            Rs =cellfun(@(x)x.Rs, in);
             if numel(unique(Rs))>1, robolog('Symbol rates must be equal', 'ERR'); end
             Fs =cellfun(@(x)x.Fs, in);       %cellfun(@(x)x.N, in) returns an array with each signal's number of elements
             if numel(unique(Fs))>1, robolog('Sample rates must be equal', 'ERR'); end
             L =cellfun(@(x)x.L, in);       %cellfun(@(x)x.N, in) returns an array with each signal's number of elements
-            if numel(unique(L))>1, robolog('Waveform lengths', 'ERR'); end 
+            if numel(unique(L))>1, robolog('Waveform lengths', 'ERR'); end
         end
     end
 end
