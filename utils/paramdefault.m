9e27c9aaab94838cc926d9383f250e9758842d87
Molly Piels <mopi@fotonik.dtu.dk>
2016-09-05 14:27:07 +0200

    Reference unit::setparams in comments for (deprecated) paramdefault

--- utils/paramdefault.m
+++ utils/paramdefault.m
@@ -2,6 +2,12 @@
 %> @brief Set default parameter values
 %> 
 %> @ingroup roboUtils
+%>
+%> __WARNING__
+%> The embedded method _setparams_ in the class _unit_ should be used instead of _paramdefault_ when possible,
+%> it simplifies the code a lot.
+
+%> @brief Set default parameter values
 %> 
 %> Helper function that defaults any param to a given value, in case it
 %> doesn't exist. In case several params are present, it takes the last.
