5038158a3d5dba9cf640ebb1cf6cae197b9a60f7
Molly Piels <mopi@fotonik.dtu.dk>
2016-09-05 14:28:02 +0200

    More concise

--- utils/findrange.m
+++ utils/findrange.m
@@ -3,12 +3,5 @@ function [minmax,minmax_struct] = findrange(x)
 
 min_ = min(x);
 max_ = max(x);
-if ~isscalar(min_) || ~isscalar(max_)
-    for i=2:ndims(x)
-        min_ = min(min_);
-        max_ = max(max_);
-    end
-end
-
+minmax = [min_; max_];
 minmax_struct = struct('min',min_,'max',max_);
-minmax = [min_ max_];
