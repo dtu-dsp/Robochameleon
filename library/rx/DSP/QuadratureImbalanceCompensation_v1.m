6de3e6bb17ac8e0296a0d209347327654623661d
Molly Piels <mopi@fotonik.dtu.dk>
2016-09-05 16:42:36 +0200

    Save correlation as a result

--- library/rx/DSP/QuadratureImbalanceCompensation_v1.m
+++ library/rx/DSP/QuadratureImbalanceCompensation_v1.m
@@ -21,24 +21,6 @@ classdef QuadratureImbalanceCompensation_v1 < unit
         
     end
     
-    methods (Static)
-        %> @brief IQ imbalance compensation routine
-        %> 
-        %> @param Ein complex baseband signal to be corrected 
-        %> @retval Eout corrected and normalized signal
-        function Eout=quadimb(Ein)
-            I = real(Ein);
-            Q = imag(Ein);
-            
-            rho = mean(I.*Q);
-            P_I =pwr.meanpwr(I);
-            Q = Q - rho*I/P_I;
-            P_Q = pwr.meanpwr(Q);
-            
-            Eout = pwr.normpwr(I/sqrt(P_I) + 1j*Q/sqrt(P_Q));
-        end
-    end
-    
     methods
         %> @brief Class constructor
         %>
@@ -49,10 +31,31 @@ classdef QuadratureImbalanceCompensation_v1 < unit
         %> @brief Main function
         function out = traverse(obj,in)
             
+            obj.results.rho = nan(in.N,1);
             out=fun1(in, @(x) obj.quadimb(x));
             
             out = signal_interface(getRaw(out), struct('Rs', in.Rs, 'Fs', in.Fs, 'Fc', in.Fc, 'P', in.P));
                         
+        end
+        
+        %> @brief IQ imbalance compensation routine
+        %> 
+        %> @param Ein complex baseband signal to be corrected 
+        %> @retval Eout corrected and normalized signal
+        function Eout=quadimb(obj, Ein)
+            I = real(Ein);
+            Q = imag(Ein);
+            
+            rho = mean(I.*Q);
+            P_I =pwr.meanpwr(I);
+            Q = Q - rho*I/P_I;
+            P_Q = pwr.meanpwr(Q);
+            
+            Eout = pwr.normpwr(I/sqrt(P_I) + 1j*Q/sqrt(P_Q));
+            
+            %save imbalance
+            idx = find(isnan(obj.results.rho), 1);
+            obj.results.rho(idx) = rho;
         end
        
         
