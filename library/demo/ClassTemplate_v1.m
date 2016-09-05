e500da2c04c251dd9b5ede7b1829c70b762825d1
Molly Piels <mopi@fotonik.dtu.dk>
2016-09-05 14:45:25 +0200

    Template improvements

--- library/demo/ClassTemplate_v1.m
+++ library/demo/ClassTemplate_v1.m
@@ -1,34 +1,32 @@
 %> @file ClassTemplate_v1.m
-%> @brief A short description of the class
+%> @brief A short description of the file containing a class or function
+
+%>@class ClassTemplate_v1
+%>@brief A short description of the class
 %>
-%> @class ClassTemplate_v1
-%> @brief A short description of the class
-%>
-%> @ingroup {base|stableDSP|physModels|roboUtils|coreDSP}
+%> @ingroup {base|stableDSP|physModels|roboUtils|coreDSP|utils|labTools}
 %> 
+%> This unit is based on the model described in \ref BookAuthor1 "[1, section 2]".
 %> A longer description of the class, with all the details required which spans multiple lines
 %> A longer description of the class, with all the details required which spans multiple lines
 %> A longer description of the class, with all the details required which spans multiple lines
 %>
-%>
-%> __Observations:__
+%> __Observations__
 %>
 %> 1. First observation
 %>
 %> 2. Second observation
 %>
 %>
-%> __Conventions:__
+%> __Conventions__
 %> * First convention
 %> * Second convention
 %>
 %>
-%> __Example:__
+%> __Example__
 %> @code
-%>   % Here we put a FULLY WORKING example
+%>   % Here we put a FULLY WORKING example using the MINIMUM set of required parameters
 %>   param.classtemp.param1      = 2;
-%>   param.classtemp.param2      = 'test';
-%>   param.classtemp.param3      = false;
 %>
 %>   clTemp = ClassTemplate_v1(param.classtemp);
 %>
@@ -37,17 +35,35 @@
 %>   param.sig.Fc = 193.1e12;
 %>   param.sig.Rs = 10e9;
 %>   param.sig.PCol = [pwr(20,{-2,'dBm'}), pwr(-inf,{-inf,'dBm'})];
-%>   Ein = rand(1000,2);
-%>   sIn = signal_interface(Ein, param.sig);
+%>   sIn = createDummySignal();
 %>
-%>   sigOut = clTemp.traverse(sigIn);
+%>   sigOut = clTemp.traverse(sIn);
 %> @endcode
 %>
 %>
-%> __References:__
+%> __Advanced Example (Optional)__
+%> @code
+%>   % Here we put a FULLY WORKING example using a more extended set of parametersedit 
+%>   param.classtemp.param1      = 2;
+%>   param.classtemp.param2      = 'test';
+%>   param.classtemp.param3      = false;
 %>
-%> * [1] First formatted reference
-%> * [2] Second formatted reference
+%>   clTemp = ClassTemplate_v1(param.classtemp);
+%>
+%>   param.sig.Fs = 64e9;
+%>   param.sig.Fc = 193.1e12;
+%>   param.sig.Rs = 10e9;
+%>   param.sig.PCol = [pwr(20,{-2,'dBm'}), pwr(-inf,{-inf,'dBm'})];
+%>   Ein = rand(1000,2);
+%>   sIn = signal_interface(Ein, param.sig);
+%>
+%>   sigOut = clTemp.traverse(sIn);
+%> @endcode
+%>
+%> __References__
+%>
+%> * \anchor BookAuthor1 [1] First formatted reference
+%> * \anchor BookAuthor2 [2] Second formatted reference
 %>
 %> @author Author 1
 %> @author Author 2
@@ -56,16 +72,21 @@
 classdef ClassTemplate_v1 < unit
 
     properties        
-        %> Description of param1 [measurment unit]. (Don't say anythign about default values, they go in the constructor)
+        %> Description of param1 [measurment unit]. (Don't say anything about default values, they go in the constructor)
         param1 = 5
-        %> Description of param2 [measurment unit].       
+        %> Description of param2 [measurment unit]. (Parameters should start in lower case).
         param2
-        %> Description of param3 which is a flag that can take values true or false
+        %> Description of param3Enabled which is a flag that can take values true or false. Append the Enabled suffix.
         param3Enabled
         %> Number of inputs
         nInputs = 1;
         %> Number of outputs
         nOutputs = 1;        
+    end
+    
+    properties(GetAccess=public, SetAccess=protected)
+        %> Internal variables that shuld not be set by the user
+        myProp;
     end
     
     methods (Static)
@@ -79,8 +100,8 @@ classdef ClassTemplate_v1 < unit
         %> Constructs an object of type ClassTemplate_v1 and more information..
         %> Don't put example here, since we have it in the description..
         %>        
-        %> @param param.param1          Param1 description (capital first letter) [unit]. [Default: Value]
-        %> @param param.param2          Param2 description (capital first letter) [unit].
+        %> @param param.param1          Description of param1 (Start description sentence with capital letter) [unit]. [Default: Value]
+        %> @param param.param2          Description of param2 (Start description sentence with capital letter) [unit].
         %> @param param.param3Enabled   Description of a flag to enable or disable something. [Default: Value]
         %> 
         %> @retval obj      An instance of the class ClassTemplate_v1
---
+++ library/demo/functionTemplate_v1.m
@@ -0,0 +1,57 @@
+%> @file functionTemplate_v1.m
+%> @brief A short description of the general idea behind the function
+%>
+%> @ingroup {base|stableDSP|physModels|roboUtils|coreDSP|utils}
+%>
+%> Information concerning the function, but not specific on the function implementation.
+%> See robolog.m for an example. The implementation is based on [\ref BookAuthor1].
+%>
+%>
+%> __Observations__
+%>
+%> 1. First observation
+%>
+%> 2. Second observation
+%>
+%>
+%> __Conventions__
+%> * First convention
+%> * Second convention
+%>
+%>
+%> __Example__
+%> @code
+%>   % Here we put a FULLY WORKING example
+%>   param1 = XX;
+%>   param2 = YY;
+%>   output = myFunction(param1, param2);
+%> @endcode
+%>
+%>
+%> __References__
+%>
+%> * \anchor BookAuthor1 [1] First formatted reference
+%> * \anchor BookAuthor2 [2] Second formatted reference
+%>
+%> @author Author 1
+%> @author Author 2
+%>
+%> @see robolog.m
+%>
+%> @version 1
+
+%> @brief Brief description of the function
+%>
+%> More detailed description of the function. Examples should not go here.
+%>
+%> @param param1          Param1 description (capital first letter) [unit]. [Default: Value]
+%> @param param2          Param2 description (capital first letter) [unit].
+%> @param param3Enabled   Description of a flag to enable or disable something. [Default: Value]
+%>
+%> @retval output1 Output value 1
+%> @retval output2 Output value 1
+function [output1, output2] = functionTemplate_v1(param1, param2, param3Enabled)
+
+% Do something here
+
+end
