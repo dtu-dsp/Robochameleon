534142513f74f38704775890f3a94c1efe34268d
Molly Piels <mopi@fotonik.dtu.dk>
2016-09-05 14:14:10 +0200

    Improve documentation for hard decision

--- utils/constutils/hd_euclid.m
+++ utils/constutils/hd_euclid.m
@@ -1,15 +1,24 @@
-function [symb,dist] = hd_euclid(X,c)
+%> @file hd_euclid.m
+%> @brief Minimun euclidean distance symbol decisor
+%>
+%> @author Robert Borkowski <rbor@fotonik.dtu.dk>
+%> @version 1
+
+%> @brief Euclidean metric hard decision digital demodulation
+%>
+%> To output is returned as unit16 by default. To get the output as double
+%> call the function as follow.
+%> @code
+%> out = double(hd_euclid(X, c));
+%> @endcode
+%>
+%> @param X Data to be decided, complex symbols
+%> @param c Constellation, complex symbols (vector)
+%>
+%> retval symb Demodulated symbols [unint16]
+%> retval dist Distance of the symbols form the closest point in the reference constellation
+function [symb,dist] = hd_euclid(X, c)
 % Euclidean metric hard decision digital demodulation
-%
-% SYMB = DD_HARD(X,C)
-%
-%   X - input constellation points
-%   C - reference constellation
-%   SYMB - demodulated symbols
-%
-% Robert Borkowski, rbor@fotonik.dtu.dk
-% Technical University of Denmark
-% v2.0, 15 August 2014
 
 [dist,symb] = min(abs(bsxfun(@minus,X(:).',c(:))));
 symb = uint16(symb(:));
