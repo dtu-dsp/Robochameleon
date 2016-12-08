602e193d2929d8d3de9a594809a79fb45740bde1
Molly Piels <mopi@fotonik.dtu.dk>
2016-12-08 15:33:22 +0100

    Update robohelp
    Better comments and error handling

--- utils/robotools/robohelp.m
+++ utils/robotools/robohelp.m
@@ -1,18 +1,37 @@
 %> @file robohelp.m
-%> @brief Open the robochameleon help
+%> @brief Open the robochameleon help main page or a class/function documentation in the browser.
 %>
-%> __Notes:__
+%> @ingroup roboUtils
+%>
+%> __Notes__
 %>
 %> The system browser must be specified in Preferences > MATLAB > web.
 %>
-%> @brief Open the main page of the doxygen robochameleon help in the system browser
-%> 
-%> Open the index page of the robochmeleon help if no argument specified.
+%> __Example__
+%>
+%> @code
+%>   % Open the help main page
+%>   robohelp
+%>
+%>   % Open class help page
+%>   robohelp NonlinearChannel_v3
+%>
+%>   % Open class help page (partial match)
+%>   robohelp BERT
+%> @endcode
+%>
+%> @author Simone Gaiarin
+%>
+%> @version 1
+
+%> @brief Open the robochameleon help main page or a class/function documentation in the browser.
+%>
+%> Open the main page of the robochmeleon help if no arguments are specified.
 %> If a search string is specified, it tries to find a class/file containing that string.
 %> An extra parameter containing 'c' can be specified to limit the search to classes only.
 %>
-%> @param varargin{1} Query string
-%> @param varargin{2} Type. Possible values = {'c' = Class}
+%> @param varargin{1} Query string.
+%> @param varargin{2} Type. Possible values = {'c' = Class}.
 function robohelp(varargin)
     roboRoot = getpref('robochameleon','roboRootFolder','NULL');
     if strcmp(roboRoot,'NULL')
@@ -22,6 +41,7 @@ function robohelp(varargin)
     excludePaths = '\.git|user manual'; %Regexp format
     if ~isempty(varargin)
         searchTerm = varargin{1};
+        searchTerm = strrep(searchTerm, '*', '.*'); % * match all, shell style
         classOnly = 0;
         if length(varargin) > 1
             if strcmp(varargin{2}, 'c')
@@ -37,15 +57,23 @@ function robohelp(varargin)
                 if ~isempty(regexp(namee, searchTerm, 'once'))
                     if classOnly && ~(exist(namee, 'class') == 8)
                         continue;
-                    end                    
-                    if isempty(fullName)
+                    end
+                    if isempty(fullName) || strcmp(namee, searchTerm)
+                        % Do this on the first match and also if the file name matches COMPLETELY the
+                        % searchTerm
                         fullName = namee;
+                    elseif isempty(regexp(searchTerm, '[tTest]', 'once')) && ...
+                            ~isempty(regexp(namee, '[tT]est', 'once'))
+                        % This will avoid the unit testers to match as duplicate, but if we are looking for a
+                        % unit tester it will fail if there are multiple matches of the search term.
+                        continue;
                     else
-                        robolog('Search query matches multiple classes/files', 'ERR');
-                    end                    
+                        robolog('Search query matches multiple classes/files. Ignoring %s.', 'WRN', namee);
+                    end
                 end
             end
         end
+        robolog('Opening documentation for class %s', fullName);
         if isempty(fullName)
             robolog('Search query didn''t match anything.', 'ERR');
         end
